77e3952345049131d8a43a459dbd4e51fff254bd
Nath <nathcf1998@gmail.com>
2019-10-17 16:18:34 -0300

    first commit

---
+++ ViewController/AcharPancViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AcharPanc.swift
+//  ProjetoPanc
+//
+//  Created by Natalia Cruz on 17/10/19.
+//  Copyright © 2019 Natalia Cruz. All rights reserved.
+//
+
+import UIKit
+
+class AcharPancViewController: UIViewController {
+    
+    @IBOutlet weak var pancTableView: UITableView!
+    
+    @IBOutlet weak var ilustracaoPanc: UIImageView!
+    @IBOutlet weak var nomePanc: UILabel!
+    @IBOutlet weak var nomeCientificoPanc: UILabel!
+    
+    let beldroega = Panc (ilustracao: UIImage, nome: "Beldroega", nomecientifico: "Portulaca Oleraceae")    
+    let capeba = Panc (ilustracao: UIImage, nome: "Capeba Pequena", nomecientifico: "Piper Marginatum")
+    let tamarindo = Panc (ilustracao: UIImage, nomePanc: "Tamarindo", nomecientifico: "Tamarindus Indica")
+    
+    lazy var pancs: [Panc] = [beldroega, capeba, tamarindo]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
---
+++ ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjetoPanc
+//
+//  Created by Natalia Cruz on 17/10/19.
+//  Copyright © 2019 Natalia Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
